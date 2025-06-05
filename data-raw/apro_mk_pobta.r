1e257bd29ae17d4bf2ba14fa8dff9beef6d569c2
HIMICS Mihaly (AGRI) <Mihaly.HIMICS@ec.europa.eu>
2025-06-05 17:36:01 +0200

    Added part to compare latest data download from Eurostat to earlier ones

--- data-raw/apro_mk_pobta.r
+++ data-raw/apro_mk_pobta.r
@@ -19,7 +19,7 @@ library(restatapi)
 #
 
 # output folder on U: drive
-extraction_folder <- "U:/4-Market Analysis/4-2 Short-Term Outlook/Outlook Dairy/Short term dairy/2024_2/Eurostat download with R/"
+extraction_folder <- "U:/4-Market Analysis/4-2 Short-Term Outlook/Outlook Dairy/Short term dairy/2025_1/Eurostat download with R/"
 
 
 apro_mk_pobta <- get_eurostat(id="apro_mk_pobta")
@@ -73,4 +73,41 @@ write.xlsx(timestamp, file = paste(extraction_folder,"apro_mk_pobta_fromR.xlsx",
 # save data extraction also in R data format
 save(apro_mk_pobta, file = paste(extraction_folder,"apro_mk_pobta_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
 save(apro_mk_pobta_dic, file = paste(extraction_folder,"apro_mk_pobta_dic_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+
+
+# PART II - Check for data updates at Eurostat server
+#====================================================
+
+rm(apro_mk_pobta)
+
+# load the old file... 
+#load(file = paste(extraction_folder,"apro_mk_pobta_2024-07-02.RData",sep = ""))
+load(file = paste("U:/4-Market Analysis/4-2 Short-Term Outlook/Outlook Dairy/Short term dairy/2024_2/Eurostat download with R/",
+                  "apro_mk_pobta_2024-08-27.RData",sep = ""))
+old <- apro_mk_pobta
+rm(apro_mk_pobta)
+
+# save the new apro_mk_pobta dataset on 'new'
+load(file = paste(extraction_folder,"apro_mk_pobta_2025-06-05.RData",sep = ""))
+new <- apro_mk_pobta
+rm(apro_mk_pobta)
+
+# merge the old and the new versions and check what was updated
+x <- new %>% left_join(old, by = c("varname","varlabel","dairyprod","milkitem","geo","time"))
+data_update1 <- x %>% filter(!is.na(value.x)) %>% filter(is.na(value.y))
+data_update2 <- x %>% filter(value.x!=value.y)
+data_update3 <- x %>% filter(is.na(value.x)) %>% filter(!is.na(value.y))
+
+data_update <- rbind(data_update1, data_update2, data_update3)
+
+# give meaningful variable names
+colnames(data_update)[7] <- "value_new"
+colnames(data_update)[8] <- "value_old"
+
+
+# save data update
+save(data_update, file = paste(extraction_folder,"apro_mk_pobta_update", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+
+# clean up
+rm(data_update, data_update1, data_update3, data_update2, x, old, new)
 
