93704ebaa7a7ca728194e002bceb324cceb57f09
HIMICS Mihaly (AGRI) <Mihaly.HIMICS@ec.europa.eu>
2025-06-08 14:04:45 +0200

    standardized output and checking updates for mk_farm and mt_lscatl

--- data-raw/apro_mk_farm.r
+++ data-raw/apro_mk_farm.r
@@ -4,6 +4,12 @@ library(eurostat)
 library(tidyverse)
 library(xlsx)
 
+
+# output folder on U: drive
+extraction_folder <- "U:/4-Market Analysis/4-2 Short-Term Outlook/Outlook Dairy/Short term dairy/2025_1/Eurostat download with R/"
+
+
+
 # DOWNLOAD DATA
 #--------------
 
@@ -57,19 +63,55 @@ excel_out <- apro_mk_farm %>% filter(time > 1989) %>% arrange(time) %>% pivot_wi
 #write.xlsx2(as.data.frame(excel_out), file = "apro_mk_farm_fromR.xlsx", row.names = FALSE, col.names = TRUE, sheetName = "apro_mk_farm")
 
 # write to Excel 
-#write.xlsx(as.data.frame(excel_out), file = "apro_mk_farm_fromR.xlsx", 
-#           row.names = FALSE, col.names = TRUE, sheetName = "apro_mk_farm",
-#           showNA = TRUE)
+write.xlsx(as.data.frame(excel_out), file = paste(extraction_folder,"apro_mk_farm_fromR.xlsx", sep = ""), 
+           row.names = FALSE, col.names = TRUE, sheetName = "apro_mk_farm",
+           showNA = TRUE)
 
 # add timestamp
 timestamp <- format(Sys.time(), "data extracted on %Y.%m.%d-%H:%M:%S")
-write.xlsx(timestamp, file = "apro_mk_farm_fromR.xlsx", 
+write.xlsx(timestamp, file = paste(extraction_folder,"apro_mk_farm_fromR.xlsx", sep = ""), 
            row.names = FALSE, col.names = TRUE, sheetName = "timestamp",
            showNA = TRUE, append = TRUE)
 
 # save data extraction also in R data format
-save(apro_mk_farm, file = "data/apro_mk_farm.RData")
-save(agriprod, file = "data/agriprod.RData")
+save(apro_mk_farm, file = paste(extraction_folder,"apro_mk_farm_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+save(agriprod, file = paste(extraction_folder,"agriprod_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
 
+
+# PART II - Check for data updates at Eurostat server
+#====================================================
+
+rm(apro_mk_farm)
+
+# load the old file... 
+#load(file = paste(extraction_folder,"apro_mk_farm_2024-07-02.RData",sep = ""))
+load(file = paste("C:/Users/himicmi/Downloads/eurostat/data/",
+                  "apro_mk_farm.RData",sep = ""))
+old <- apro_mk_farm
+rm(apro_mk_farm)
+
+# save the new apro_mk_farm dataset on 'new'
+load(file = paste(extraction_folder,"apro_mk_farm_2025-06-08.RData",sep = ""))
+new <- apro_mk_farm
+rm(apro_mk_farm)
+
+# merge the old and the new versions and check what was updated
+x <- new %>% left_join(old, by = c("varname","varlabel","dairyprod","milkitem","geo","time"))
+data_update1 <- x %>% filter(!is.na(value.x)) %>% filter(is.na(value.y))
+data_update2 <- x %>% filter(value.x!=value.y)
+data_update3 <- x %>% filter(is.na(value.x)) %>% filter(!is.na(value.y))
+
+data_update <- rbind(data_update1, data_update2, data_update3)
+
+# give meaningful variable names
+colnames(data_update)[7] <- "value_new"
+colnames(data_update)[8] <- "value_old"
+
+
+# save data update
+save(data_update, file = paste(extraction_folder,"apro_mk_farm_update", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+
+# clean up
+rm(data_update, data_update1, data_update3, data_update2, x, old, new)
 
 
--- data-raw/apro_mt_lscatl.r
+++ data-raw/apro_mt_lscatl.r
@@ -5,6 +5,11 @@ library(tidyverse)
 library(xlsx)
 library(restatapi)
 
+# output folder on U: drive
+extraction_folder <- "U:/4-Market Analysis/4-2 Short-Term Outlook/Outlook Dairy/Short term dairy/2025_1/Eurostat download with R/"
+
+
+
 # DOWNLOAD DATA
 #--------------
 
@@ -67,19 +72,56 @@ excel_out <- apro_mt_lscatl %>% filter(time > 1989) %>% arrange(time) %>% pivot_
 #write.xlsx2(as.data.frame(excel_out), file = "apro_mt_lscatl_fromR.xlsx", row.names = FALSE, col.names = TRUE, sheetName = "apro_mt_lscatl")
 
 # write to Excel 
-write.xlsx(as.data.frame(excel_out), file = "apro_mt_lscatl_fromR.xlsx", 
+write.xlsx(as.data.frame(excel_out), file = paste(extraction_folder,"apro_mt_lscatl_fromR.xlsx", sep = ""),
            row.names = FALSE, col.names = TRUE, sheetName = "apro_mt_lscatl",
            showNA = TRUE)
 
 # add timestamp
 timestamp <- format(Sys.time(), "data extracted on %Y.%m.%d-%H:%M:%S")
-write.xlsx(timestamp, file = "apro_mt_lscatl_fromR.xlsx", 
+write.xlsx(timestamp, file = paste(extraction_folder, "apro_mt_lscatl_fromR.xlsx", sep = ""),
            row.names = FALSE, col.names = TRUE, sheetName = "timestamp",
            showNA = TRUE, append = TRUE)
 
 # save data extraction also in R data format
-save(apro_mt_lscatl, file = "data/apro_mt_lscatl.RData")
-save(apro_mt_lscatl_dic, file = "data/apro_mt_lscatl_dic.RData")
+save(apro_mt_lscatl, file = paste(extraction_folder, "apro_mt_lscatl_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+save(apro_mt_lscatl_dic, file = paste(extraction_folder, "apro_mt_lscatl_dic_", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+
+
+# PART II - Check for data updates at Eurostat server
+#====================================================
+
+rm(apro_mt_lscatl)
+
+# load the old file... 
+#load(file = paste(extraction_folder,"apro_mt_lscatl_2024-07-02.RData",sep = ""))
+load(file = paste("C:/Users/himicmi/Downloads/eurostat/data/",
+                  "apro_mt_lscatl.RData",sep = ""))
+old <- apro_mt_lscatl
+rm(apro_mt_lscatl)
+
+# save the new apro_mt_lscatl dataset on 'new'
+load(file = paste(extraction_folder,"apro_mt_lscatl_2025-06-08.RData",sep = ""))
+new <- apro_mt_lscatl
+rm(apro_mt_lscatl)
+
+# merge the old and the new versions and check what was updated
+x <- new %>% left_join(old, by = c("varname","varlabel","animals","month","geo","time"))
+data_update1 <- x %>% filter(!is.na(value.x)) %>% filter(is.na(value.y))
+data_update2 <- x %>% filter(value.x!=value.y)
+data_update3 <- x %>% filter(is.na(value.x)) %>% filter(!is.na(value.y))
+
+data_update <- rbind(data_update1, data_update2, data_update3)
+
+# give meaningful variable names
+colnames(data_update)[7] <- "value_new"
+colnames(data_update)[8] <- "value_old"
+
+
+# save data update
+save(data_update, file = paste(extraction_folder,"apro_mt_lscatl_update", format(Sys.time(), "%Y-%m-%d"), ".RData", sep = ""))
+
+# clean up
+rm(data_update, data_update1, data_update3, data_update2, x, old, new)
 
 
 
